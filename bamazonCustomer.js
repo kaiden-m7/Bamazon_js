07eb10bc90c8a7cdc5931e9de43cbcb5339eb48d
Kaiden Munsey <kaidenmnsey@aol.com>
2020-06-18 11:16:48 -0400

    Adding connections to mysql files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,17 @@
 let mysql = require("mysql2");
 let inquirer = require("inquirer");
 
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Chefkaiden09",
+    databse: "bamazonDB"
+});
+
+connection.connect(function (error){
+    if (error) throw error;
+    console.log(connection.threadId);
+    displayProducts();
+});
+
